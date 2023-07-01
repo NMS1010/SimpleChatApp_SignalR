fd200c2c000e1108dfea6c9581455ef81928e2d0
MinhSon <nguyenminhson102002@gmail.com>
2023-07-01 10:46:36 +0700

    create base project structure

---
+++ social-frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './pages/client/Login';
+
+function App() {
+    return (
+        <div className="flex justify-center items-center ">
+          <Login />
+        </div>
+    );
+}
+
+export default App;
